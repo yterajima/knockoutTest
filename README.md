d6c08293c3eec4997717f13539d7f3e9425516cb
yterajima <terra@e2esound.com>
2012-08-13 17:48:50 +0900

    Added データバインドのサンプル

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 knockoutJS: [http://knockoutjs.com/][url]
 
+* simple\_bindings.html: データバインドして表示のみ
+
 [url]:http://knockoutjs.com/
---
+++ js/simpleBindingsViewModel.js
@@ -0,0 +1,8 @@
+var SimpleBindingsViewModel = function () {
+    "use strict";
+
+    this.name    = 'yterajima';
+    this.address = 'Gunma'
+};
+
+ko.applyBindings(new SimpleBindingsViewModel());
