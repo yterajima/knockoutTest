29c8d4dcc80274cd0b81d7b5cdc8dcb43ce82121
yterajima <terra@e2esound.com>
2012-08-20 00:49:47 +0900

    markdown.jsと組み合わせてMarkdownエディタ

---
+++ js/markdownViewModel.js
@@ -0,0 +1,11 @@
+var markdownViewModel = function () {
+    "use strict";
+
+    this.textData = ko.observable('edit markdown');
+
+    this.displayHtml = ko.computed(function () {
+        return markdown.toHTML(this.textData());
+    }, this);
+};
+
+ko.applyBindings(new markdownViewModel());
